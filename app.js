1bc7ec588d0a621d8db4e4b4cb76fb04bb10ec8e
Lucas Dias <112500097+eilucasdias@users.noreply.github.com>
2024-05-27 09:08:37 -0300

    Minor changes made to the code

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express();
 const port = 2464;
 
-// Servir arquivos estáticos das pastas 'public', 'css', 'js', e 'pages'
+// Servir arquivos estáticos das pastas 'public', 'css', 'js', 'assets', e 'pages'
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 
 app.get('/', (req, res) => {
